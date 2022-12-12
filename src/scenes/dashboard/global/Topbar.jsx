ae83bfbfa0535e41e7460e0299ea96b6b99604a2
Aman <aakhoja@github.com>
2022-12-11 18:11:27 -0600

    working on light and dark mode

--- src/scenes/dashboard/global/Topbar.jsx
+++ src/scenes/dashboard/global/Topbar.jsx
@@ -31,7 +31,7 @@ const Topbar = () => {
         {/*ICONS */}
         <Box display="flex">
             <IconButton onClick={colorMode.toggleColorMode}>
-                {theme.palette.mode === 'dark' ? (<DarkModeOutlinedIcon />) : (<LightModeOutlinedIcon/>)}
+                {theme.palette.mode === "dark" ? (<DarkModeOutlinedIcon />) : (<LightModeOutlinedIcon/>)}
             </IconButton>
             <IconButton>
                 <NotificationsOutlinedIcon />
