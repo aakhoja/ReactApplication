fe51d5907a7858e7386537955353c6419a4820c4
Aman <aakhoja@github.com>
2022-12-16 00:01:38 -0600

    working on sidebar implementation

--- src/scenes/dashboard/global/Sidebar.jsx
+++ src/scenes/dashboard/global/Sidebar.jsx
@@ -16,6 +16,7 @@ import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutl
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import { BarItem } from "@nivo/bar";
 
 const Sidebar = () => {
     const theme = useTheme();
@@ -42,7 +43,83 @@ const Sidebar = () => {
                     color: "#6870fa !important",
                 },
             }}
-        ></Box>
+        >
+          <ProSideBar collapsed={isCollapsed}>
+                <Menu iconShape="square">
+                    {/* Logo and Menu Item */}
+                    <MenuItem
+                        onClick={() => setIsCollapsed(!isCollapsed)}
+                        icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+                        style={{
+                            margin: "10px 0 20px 0",
+                            color: colors.grey[100],
+                        }}
+                    >
+                        {!isCollapsed && (
+                            <Box 
+                            display="flex"
+                            justifyContent="space-between"
+                            alignItems="center"
+                            m1="15px"
+                            >
+                                <Typography variant="h3" color={colors.grey[100]}>
+                                    ADMINS
+                                </Typography>
+                                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                                    <MenuOutlinedIcon />
+                                </IconButton>
+                            </Box>
+                        )}
+                    </MenuItem>
+
+                    {!isCollapsed && (
+                        <Box mb="25px">
+                            <Box display="flex" justifyContent="center" alignItems="center">
+                                <img
+                                    alt="profile-user"
+                                    width="100px"
+                                    height="100px"
+                                    src={'../../assets/user.png'}
+                                    style={{ cursor: "pointer", borderRadius: "50%"}}
+                                    />
+                            </Box>
+
+                            <Box textAlign="center">
+                                <Typography 
+                                    variant="h2"
+                                    color={colors.grey[100]}
+                                    fontWeight="bold"
+                                    sx={{m:"10px 0 0 0"}}
+                                >
+                                    Aman Khoja
+                                </Typography>
+                            </Box>
+                        </Box>
+                    )}
+
+                    <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+                        <Item 
+                            title="Dashboard"
+                            to="/"
+                            icon={<HomeOutlinedIcon />}
+                            selected = {selected}
+                            setSelected = {setSelected}
+                        />
+
+                        <Typography
+                            variant="h6"
+                            color={colors.grey[300]}
+                            sx={{ m: "15px 0 5px 20px"}}
+                        >
+                            Data
+                        </Typography>
+
+                        <Item
+                            title="Manage Team"
+                    </Box>
+                </Menu>
+            </ProSideBar>  
+        </Box>
     );
 };
 
